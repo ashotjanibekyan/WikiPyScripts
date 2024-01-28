dc5d40dd7a8e6f5e63c4b39c3834da9ff0046704
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-01-28 20:26:25 +0400

    empty_pages.py, add very short pages

--- empty_pages.py
+++ empty_pages.py
@@ -1,16 +1,20 @@
+import re
+
 import toolforge
 import pywikibot as pw
+
+import helpers
 from helpers import nsMap
 
 conn = toolforge.connect('hywiki')
 hywiki = pw.Site('hy', 'wikipedia')
 
+sql_empty = 'SELECT page_title, page_namespace FROM page WHERE page_len = 0 AND page_namespace != 2 and page_namespace != 3 ORDER BY page_title;'
+sql_very_short = 'SELECT page_title, page_namespace, page_len FROM page WHERE page_len > 0 AND page_len <= 10 AND page_namespace != 2 and page_namespace != 3 ORDER BY page_title;'
 
-sql = 'SELECT page_title, page_namespace FROM page WHERE page_len = 0 AND page_namespace != 2 and page_namespace != 3 ORDER BY page_title;'
-
+text = 'Ցանկում ներառված են Հայերեն Վիքիպեդիայի բոլոր դատարկ էջերը՝ բացառությամբ այն էջերը, որոնք մասնակից մասնակցի քննարկում անվանատարածքներում են։\n'
 with conn.cursor() as cur:
-    text = 'Ցանկում ներառված են Հայերեն Վիքիպեդիայի բոլոր դատարկ էջերը՝ բացառությամբ այն էջերը, որոնք մասնակից մասնակցի քննարկում անվանատարածքներում են։\n'
-    cur.execute(sql)
+    cur.execute(sql_empty)
     results = cur.fetchall()
     for r in results:
         line = f"# [[{nsMap[r[1]]}:{r[0].decode('utf-8')}]]\n"
@@ -18,6 +22,22 @@ with conn.cursor() as cur:
             line = line.replace('# [[', '# [[:')
         text += line
 
-    page = pw.Page(hywiki, "Մասնակից:ԱշոտՏՆՂ/ցանկեր/դատարկ էջեր")
-    page.text = text
-    page.save("թարմացում")
+with conn.cursor() as cur:
+    cur.execute(sql_very_short)
+    results = cur.fetchall()
+    table = [['Էջ', 'Չափ']]
+    for r in results:
+        title = f"{nsMap[r[1]]}:{r[0].decode('utf-8')}"
+        page = pw.Page(hywiki, title)
+        if page.exists() and r[1] == 1 and re.match(r'\{\{[^}]+}}', page.text):
+            continue
+        line = f"[[{title}]]\n"
+        if nsMap[r[1]] == 'Պատկեր' or nsMap[r[1]] == 'Կատեգորիա':
+            line = line.replace('# [[', '# [[:')
+        table.append([line, r[2]])
+
+    text += '\n== Շատ փոքր էջեր (1-10 բայթ) ==\n' + helpers.matrix_to_wikitable(table)
+
+page = pw.Page(hywiki, 'Մասնակից:ԱշոտՏՆՂ/ցանկեր/դատարկ էջեր')
+page.text = text
+page.save('թարմացում')
