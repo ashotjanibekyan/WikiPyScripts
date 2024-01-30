d561141cceabcafa283a45d0f9fca7ab8144e885
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-01-31 00:23:52 +0400

    unneeded scripts

--- monthly.sh
+++
@@ -1,5 +0,0 @@
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py $HOME/WikiPyScripts/potd
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py $HOME/WikiPyScripts/dead_people_without_image.py
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py $HOME/WikiPyScripts/math_project_views
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py $HOME/WikiPyScripts/short_articles
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py $HOME/WikiPyScripts/vital_subject
