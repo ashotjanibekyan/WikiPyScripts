2e2f358e7d0ba699886fcfa9c7c9819f0fdd1b29
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-02-16 12:36:03 +0400

    Add films_without_images.py

---
+++ films_without_images.py
@@ -0,0 +1,47 @@
+import toolforge
+import pywikibot as pw
+
+conn = toolforge.connect('hywiki')
+hywiki = pw.Site('hy', 'wikipedia')
+
+page = pw.Page(hywiki, 'Վիքիպեդիա:Ցանկեր/ֆիլմեր, որոնք պատկեր չունեն')
+
+query = '''WITH NonFreeImages AS
+  (SELECT page_title AS title
+   FROM page AS p
+   JOIN categorylinks cl ON p.page_id = cl.cl_from
+   WHERE cl.cl_to = 'Բոլոր_ոչ_ազատ_պատկերներ'),
+     Movies AS
+  (SELECT page_id AS movie_id
+   FROM page AS p
+   JOIN categorylinks cl ON p.page_id = cl.cl_from
+   WHERE cl.cl_to = 'Ֆիլմեր_այբբենական_կարգով'
+     AND p.page_namespace = 0),
+     MoviesWithNonFreeImages AS
+  (SELECT page_id AS movie_with_non_free_image_id
+   FROM page p
+   JOIN Movies AS m ON p.page_id = m.movie_id
+   JOIN imagelinks il ON p.page_id = il.il_from
+   WHERE p.page_is_redirect = 0
+     AND p.page_namespace = 0
+     AND EXISTS (
+       SELECT 1
+       FROM NonFreeImages nf
+       WHERE il.il_to = nf.title))
+SELECT concat('# [[', p.page_title, ']]')
+FROM page p
+JOIN Movies m ON p.page_id = m.movie_id
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM MoviesWithNonFreeImages mn
+    WHERE p.page_id = mn.movie_with_non_free_image_id)
+ORDER BY p.page_title'''
+
+with conn.cursor() as cur:
+    text = ''
+    cur.execute(query)
+    results = cur.fetchall()
+    for r in results:
+        text += '\n' + r[0].decode('utf-8').replace('_', ' ')
+    page.text = text
+    page.save(summary='թարմացում', botflag=False)
