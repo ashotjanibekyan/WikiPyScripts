c7292bfb68985edd0843a5cc678c1ded9145c63c
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-02-19 12:21:09 +0400

    optimize math_project_adder

--- math_project_adder.py
+++ math_project_adder.py
@@ -1,12 +1,40 @@
-import pywikibot as pw
 import re
+import toolforge
+import pywikibot as pw
 
-from helpers import convert_to, get_wikipedias
+enwiki = pw.Site('en', 'wikipedia')
+hywiki = pw.Site('hy', 'wikipedia')
 
-hywiki, enwiki = get_wikipedias('hy', 'en')
+enconn = toolforge.connect('enwiki')
+hyconn = toolforge.connect('hywiki')
 
-temp = pw.Page(enwiki, 'Template:WikiProject Mathematics')
-gen = temp.getReferences(only_template_inclusion=True, namespaces=1, follow_redirects=True)
+en_query = '''WITH MathTitle AS
+  (SELECT page_title
+   FROM page
+   JOIN categorylinks ON cl_from = page_id
+   WHERE cl_to in ('High-priority_mathematics_articles',
+                   'Low-priority_mathematics_articles',
+                   'Mid-priority_mathematics_articles',
+                   'NA-priority_mathematics_articles',
+                   'Top-priority_mathematics_articles',
+                   'Unknown-priority_mathematics_articles')
+   LIMIT 30000)
+SELECT DISTINCT ll_title,
+                p1.page_title
+FROM page p1
+JOIN langlinks ON ll_from = p1.page_id
+WHERE page_namespace = 0
+  AND ll_lang = 'hy'
+  AND EXISTS
+    (SELECT 1
+     FROM MathTitle
+     WHERE p1.page_title = page_title);'''
+
+hy_query = '''SELECT page_title
+FROM page
+JOIN categorylinks ON cl_from = page_id
+WHERE cl_to = 'Մաթեմատիկական_հոդվածներ'
+LIMIT 30000;'''
 
 fields = {
     'algebra': 'հանրահաշիվ',
@@ -86,21 +114,34 @@ def create_hy_template(text):
         return hytemplate
     return None
 
+hy_en_map = {}
+with enconn.cursor() as cur:
+  cur.execute(en_query)
+  results = cur.fetchall()
+  for r in results:
+    hy_title = r[0].decode('utf-8')
+    en_title = r[1].decode('utf-8')
+    hy_en_map[hy_title] = en_title
 
-for p in gen:
-    skipped = False
-    en_page = p.toggleTalkPage()
-    hy_page, _ = convert_to(en_page, hywiki)
-    if hy_page:
-        hy_template = create_hy_template(p.text)
-        if not hy_template:
-            skipped = True
-            continue
-        hy_page_talk = hy_page.toggleTalkPage()
-        hy_page_talk.text = re.sub(r'\{\{Վիքինախագիծ Մաթեմատիկա}}\n?', '', hy_page_talk.text)
-        if '{{Վիքինախագիծ Մաթեմատիկա' not in hy_page_talk.text:
-            hy_page_talk.text = hy_template + '\n' + hy_page_talk.text
-            hy_page_talk.save(
-                summary='+' + hy_template + ', ըստ [[Special:PermaLink/7367323#{{Վիքինախագիծ_Մաթեմատիկա}}_կաղապարը_քննարկման_էջերում]]')
-        else:
-            skipped = True
+already_has = set()
+with hyconn.cursor() as cur:
+  cur.execute(hy_query)
+  results = cur.fetchall()
+  for r in results:
+    already_has.add(r[0].decode('utf-8').replace('_', ' '))
+
+for hy_title in hy_en_map:
+  if hy_title not in already_has:
+    hy_page = pw.Page(hywiki, hy_title)
+    en_talk_page = pw.Page(enwiki, 'Talk:' + hy_en_map[hy_title])
+    if not en_talk_page or not en_talk_page.exists():
+      continue
+    hy_template = create_hy_template(en_talk_page.text)
+    if not hy_template:
+      continue
+    hy_page_talk = hy_page.toggleTalkPage()
+    hy_page_talk.text = re.sub(r'\{\{Վիքինախագիծ Մաթեմատիկա}}\n?', '', hy_page_talk.text)
+    if '{{Վիքինախագիծ Մաթեմատիկա' not in hy_page_talk.text:
+        hy_page_talk.text = hy_template + '\n' + hy_page_talk.text
+        hy_page_talk.save(
+            summary='+' + hy_template + ', ըստ [[Special:PermaLink/7367323#{{Վիքինախագիծ_Մաթեմատիկա}}_կաղապարը_քննարկման_էջերում]]')
