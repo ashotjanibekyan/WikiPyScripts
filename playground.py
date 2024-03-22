59a851489fbe19b6ad8c25db03d6b18298878c76
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-03-22 11:46:49 +0400

    tests

--- playground.py
+++ playground.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import helpers
 
 
-def main(project, delta, gen):
+def main(project, delta, gen, prf):
     site = pw.Site('hy', project)
     end = str(site.server_time() - delta)
 
@@ -32,16 +32,20 @@ def main(project, delta, gen):
             else:
                 break
 
-    list_page = pw.Page(site, f'{project}:Ցանկեր/վերջին անգամ խմբագրել է IP')
-    list_page.text = helpers.matrix_to_wikitable(last_edited_by_ip)
-    list_page.save('թարմացում', botflag=False)
+    list_page = pw.Page(site, f'{project}:Ցանկեր/վերջին անգամ խմբագրել է IP/{prf}')
+    if len(last_edited_by_ip) > 1:
+        list_page.text = helpers.matrix_to_wikitable(last_edited_by_ip)
+        list_page.save('թարմացում', botflag=False)
 
 
 if __name__ == '__main__':
+    namespaces = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12, 13, 14, 15, 100, 101, 102, 103, 104, 105, 106, 107, 710, 711, 828, 829]
     delta = datetime.timedelta(days=365)
     project = 'wikisource'
     site = pw.Site('hy', project)
     end = str(site.server_time() - delta)
     # gen = list(set(pg.RecentChangesPageGenerator(site=site, namespaces=namespaces, end=end)))
-    pages = list(pg.AllpagesPageGenerator(site=site, includeredirects=False))
-    main(project, delta, pages)
+
+    for ns in namespaces:
+        pages = list(site.allpages(start='!', namespace=ns))
+        main(project, delta, pages, ns)
