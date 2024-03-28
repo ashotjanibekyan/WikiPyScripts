c2859f73513b7d9c1e056a1eeacc664f7aa94c3f
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-03-28 09:33:17 +0400

    math_project_adder.py, typo

--- math_project_adder.py
+++ math_project_adder.py
@@ -154,5 +154,5 @@ watchlist.text = ''
 for talk in cat.members():
     page = talk.toggleTalkPage()
     if page.namespace() == 0:
-        watchlist.text += f'#[[{page.title()}]]'
+        watchlist.text += f'#[[{page.title()}]]\n'
 watchlist.save('թարմացում')
