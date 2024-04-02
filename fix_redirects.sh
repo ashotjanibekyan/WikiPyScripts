6c03b20d0de7f36a77a9f452035b5ad24fb8e73f
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-04-02 16:47:42 +0400

    add fix_redirects.sh

---
+++ fix_redirects.sh
@@ -0,0 +1,8 @@
+pwb redirect br -delete -always -site:wikipedia -lang:hy
+pwb redirect double -always -site:wikipedia -lang:hy
+
+pwb redirect br -delete -always -site:wikipedia -lang:hyw
+pwb redirect double -always -site:wikipedia -lang:hyw
+
+pwb redirect br -delete -always -site:wiktionaty -lang:hy
+pwb redirect double -always -site:wiktionaty -lang:hy
