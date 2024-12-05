a7d5909983e8c8bb105db54b5eee67092bbebbd1
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2024-12-05 12:01:30 +0000

    disable hywikitionary broken redirect fixes

--- fix_redirects.sh
+++ fix_redirects.sh
@@ -2,6 +2,3 @@ $HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py $HOME/WikiPyScripts/fix_re
 
 $HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py redirect br -delete -always -site:wikipedia -lang:hyw -sdtemplate:"{{Ջնջել|կոտրված վերահղում}}"
 $HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py redirect double -always -site:wikipedia -lang:hyw
-
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py redirect br -delete -always -site:wiktionary -lang:hy -sdtemplate:"{{Ջնջել|կոտրված վերահղում}}"
-$HOME/pwbvenv/bin/python3 $HOME/pywikibot-core/pwb.py redirect double -always -site:wiktionary -lang:hy
